0f1985661a2052882cb95f3b873da80b0d515581
MuhammadUsman3506 <usman.gondal238@gmail.com>
2023-06-14 19:02:26 +0500

    updated

--- examples/processor/processor.sh
+++ examples/processor/processor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 create -f processor.sv	#create file
 add -P WIDTH RAM_WIDTH RAM_ADDR_BITS PROG_FILE PROG_START_ADDR PROG_END_ADDR DATA_FILE DATA_START_ADDR DATA_END_ADDR -v "9" "32" "9" \"'program.hex'\" "0" "8" \"'data_file.txt'\" "0" "5" #adding parameters
-add -r opcode -rn "[4:0]" # add reg 
+add -w opcode -rn "[4:0]" # add reg 
 add -w instr reg_data_in alu_out dram_data_out op2 op1 prog_cnt ctl3 ctl2 ctl1 -rn "[RAM_WIDTH-1:0]" "[RAM_WIDTH-1:0]" "[RAM_WIDTH-1:0]" "[RAM_WIDTH-1:0]" "[RAM_WIDTH-1:0]" "[RAM_WIDTH-1:0]" "[WIDTH-1:0]" "None" "None" "None"  #adding wire 1-bit 
 add -lp LW SW  -v "5'd4" "5'd5"  # LOCAL PARAM
 ###################### up_counter ########################
@@ -26,7 +26,7 @@ connect -w op1 op2 ctl1 reg_data_in -i regfile_inst -ip rd_data1 rd_data2 wr_en 
 #connect -lp instr_addr1 instr_addr2 wr_addr_instr -i regfile_inst -ip rd_addr1 rd_addr2 wr_addr # local parameter to instance
 connect -v "instr[21:17]" "instr[16:12]" "instr[26:22]" -i regfile_inst -ip rd_addr1 rd_addr2 wr_addr # constant value connect to the instance 
 ######################### comb block    ####################################################
-plug -i "instr[31:27]" -o "opcode" ## plug combination block
+###plug -i "instr[31:27]" -o "opcode" ## plug combination block
 ######################### ALU #####################################
 plug -inst ALU.sv -n ALU_inst                 #plug instance  ALU_inst
 add -nw RAM_WIDTH -ow RAM_WIDTH -inst ALU_inst #
@@ -42,9 +42,9 @@ connect -w ctl2 alu_out dram_data_out reg_data_in -i Mux_inst -ip s_l in0 in1 ou
 plug -inst data_mem.sv -n data_mem_inst	#plug instance data_mem_inst
 add -ow RAM_WIDTH RAM_ADDR_BITS DATA_FILE INIT_START_ADDR  INIT_END_ADDR -nw RAM_WIDTH RAM_ADDR_BITS DATA_FILE DATA_START_ADDR DATA_END_ADDR -inst data_mem_inst
 connect -i data_mem_inst -ip clock -op clk # Top_level_file inputs/outputs to instance ports
-connect -w ctl3 -i data_mem_inst -ip write_enable 
-connect -w op1 -i data_mem_inst -ip in_data 
-connect -w dram_data_out -i data_mem_inst -ip out_data 
+connect -w ctl3 -i data_mem_inst -ip write_enable # gdsfg
+connect -w op1 -i data_mem_inst -ip in_data # jghjj
+connect -w dram_data_out -i data_mem_inst -ip out_data # jgjhg
 #connect -w ctl3 op1 dram_data_out -i data_mem_inst -ip write_enable in_data out_data #--> this is commented b/c this is already done by previos CMDS # connect : wire to instance ports
 #connect -lp single_bit1 datamem_addr_instr -i data_mem_inst -ip ram_enable address #
 connect -v "1'b1" "instr[8:0]" -i data_mem_inst -ip ram_enable address # constant value connect to the instance
@@ -54,12 +54,19 @@ connect -w ctl3 ctl1 -i inst_not_gate -ip in out # wire to instance
 ############################ comparator_inst  ################
 plug -inst comparator.sv -n inst_comparator	#plug instance inst_comparator
 add -nw DATA_END_ADDR  -ow WIDTH -inst inst_comparator #
-connect -r opcode -i inst_comparator -ip in0 # register to instance 
+connect -w opcode -i inst_comparator -ip in0 # register to instance 
 connect -w ctl2 -i inst_comparator -ip out # wire to isnstance
 connect -lp LW -i inst_comparator -ip in1 # local parameter to instance
 ################################## comparator_inst1  ################
 plug -inst comparator.sv -n inst1_comparator	#plug inst1_comparator
-add -nw DATA_END_ADDR -ow  WIDTH -inst inst1_comparator #
-connect -r opcode -i inst1_comparator -ip in0 #  register to instance 
+add -nw DATA_END_ADDR -ow  WIDTH -inst inst1_comparator # 
+connect -w opcode -i inst1_comparator -ip in0 #  register to instance 
 connect -w ctl3 -i inst1_comparator -ip out # wire to isnstance
 connect -lp SW -i inst1_comparator -ip in1 # local parameter to instance
+########################  comb_block ####################### comb_block.sv
+plug -inst comb_block.sv -n inst_comb_block	#plug inst_comb_block
+add -nw DATA_END_ADDR -ow  WIDTH -inst inst_comb_block # gdfd
+##connect -r opcode -i inst_comb_block -ip out # wire to isnstance
+connect -v "instr[31:27]" "opcode" -i inst_comb_block -ip in out # constant value connect to the instance
+
+
